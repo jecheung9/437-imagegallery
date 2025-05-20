7081b6f21b0dd5177bcac1751fd1d35423ff59a8
jecheung9 <jeffcheung424@gmail.com>
2025-05-19 21:17:05 -0700

    finish lab 19

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -6,16 +6,17 @@ import { Routes, Route } from "react-router";
 import { MainLayout } from "./MainLayout.tsx";
 import { useState } from "react";
 import { fetchDataFromServer } from "./MockAppData.ts";
+import { ValidRoutes } from "../../backend/src/shared/ValidRoutes.ts"
 
 function App() {
     const [imageData] = useState(fetchDataFromServer)
     return (
         <Routes>
-            <Route path="/" element={<MainLayout />}>
+            <Route path={ValidRoutes.HOME} element={<MainLayout />}>
                 <Route index element={<AllImages imageData={imageData}/>} />
-                <Route path="upload" element={<UploadPage />} />
-                <Route path="login" element={<LoginPage />} />
-                <Route path="images/:imageId" element={<ImageDetails imageData={imageData}/>} />
+                <Route path={ValidRoutes.UPLOAD} element={<UploadPage />} />
+                <Route path={ValidRoutes.LOGIN} element={<LoginPage />} />
+                <Route path={ValidRoutes.IMAGES} element={<ImageDetails imageData={imageData}/>} />
             </Route>
         </Routes>
     )
